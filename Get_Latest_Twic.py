3145a7732b8d1235392767ced5660ce396e5b935
Jeroen van de Weijer <68313926+JMW64@users.noreply.github.com>
2020-08-11 21:53:46 +0800

    Update Get_Latest_Twic.py

--- Get_Latest_Twic.py
+++ Get_Latest_Twic.py
@@ -11,11 +11,12 @@ myheaders = {
 
 url = 'https://theweekinchess.com/twic'
 
+# Find the number of the latest issue:
 r = requests.get(url, headers=myheaders)
 html_contents = r.text
 html_soup = BeautifulSoup(html_contents, 'html.parser')
 
-# Find the first table tag, second row, first cell
+# Find the first table tag, then the second row, then the first cell:
 first_table = html_soup.find('table', {"class":"results-table"})
 secondrow = first_table.find('tr', {"class":""})
 entry = secondrow.find('td').get_text(strip=True)
@@ -23,21 +24,22 @@ entry = secondrow.find('td').get_text(strip=True)
 # concatenate webpath:
 fullpath = os.path.join(webname + r"/twic" + entry + sfxname)
 
+# optional: print fullpath to check results
 # print(fullpath)
 
-# download the file:
+# download the file, using wget
 file_name = wget.download(fullpath)
 
-# move the file
-shutil.move(file_name, "d:/pgn/twicg/new/aaa.zip")
+# move the file to a specific directory (change as required)
+shutil.move(file_name, "c:/pgn/twic/new/aaa.zip")
 
-# unzip the file
-with ZipFile("d:/pgn/twicg/new/aaa.zip", 'r') as zipObj:
-    # Extract the contents of the zip file in directory on D:
-    zipObj.extractall(path="d:/pgn/twicg/new")
+# unzip the file (change the path as required)
+with ZipFile("c:/pgn/twic/new/aaa.zip", 'r') as zipObj:
+    # Extract the contents of the zip file in directory on C:
+    zipObj.extractall(path="c:/pgn/twic/new")
 
 # delete the zip file
-os.remove("d:/pgn/twicg/new/aaa.zip")
+os.remove("c:/pgn/twic/new/aaa.zip")
 
 # print success message
-print("\n\nLatest TWIC file downloaded, unzipped and saved in the D:/PGN/Twicg/NEW folder", "\n")
+print("\n\nLatest TWIC file downloaded, unzipped and saved in the C:/PGN/Twic/NEW folder", "\n")
